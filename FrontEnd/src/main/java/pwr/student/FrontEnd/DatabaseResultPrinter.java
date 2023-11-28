6f44c7d0018a038bdf0de3567a46891d5c91b2af
cholewakrzysztof <cholewa.krzysztof24@gmail.com>
2023-11-29 00:32:17 +0100

    Fix build manifest file Try to do responsive table result

--- FrontEnd/src/main/java/pwr/student/FrontEnd/DatabaseResultPrinter.java
+++ FrontEnd/src/main/java/pwr/student/FrontEnd/DatabaseResultPrinter.java
@@ -2,21 +2,26 @@ package pwr.student.FrontEnd;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.*;
 
 public class DatabaseResultPrinter {
-    public static void printTableResult(ResultSet rs,String[] columns) throws SQLException {
+    public static void printTableResult(ResultSet rs,String[] columns) throws Exception {
         ArrayList<String> col = new ArrayList<>(Arrays.asList(columns));
         if(col.contains("*")) {
             col.remove("*");
-            col.add("component");
+            col.add("id");
             col.add("person");
+            col.add("component");
             col.add("priority");
             col.add("date");
-            col.add("id");
             col.add("description");
         }
+        Map<String,Integer> fieldWidth = getSizeOfColumns(rs,col);
+
+        //TODO read result set twice!
+        for (String key : fieldWidth.keySet())
+            System.out.println(key+" "+fieldWidth.get(key).toString());
+
         StringBuilder title = new StringBuilder();
         for (String column:col) {
             title.append(column).append("\t");
@@ -29,5 +34,26 @@ public class DatabaseResultPrinter {
             }
             System.out.println(row);
         }
+    }
+
+    private static Map<String, Integer> getSizeOfColumns(ResultSet rs, ArrayList<String> columns) throws Exception{
+            Map<String, Integer> sizes = new HashMap<>();
+            while (rs.next()) {
+                columns.forEach(colName -> {
+                    String value = null;
+                    try {
+                        value = rs.getString(colName);
+                    } catch (SQLException e) {
+                        throw new RuntimeException(e);
+                    }
+                    if (sizes.containsKey(colName))
+                        sizes.put(colName, sizes.get(colName) > value.length() ? sizes.get(colName) : value.length());
+                    else
+                        sizes.put(colName,0);
+                });
+            }
+            rs.beforeFirst();
+
+        return sizes;
     }
 }
