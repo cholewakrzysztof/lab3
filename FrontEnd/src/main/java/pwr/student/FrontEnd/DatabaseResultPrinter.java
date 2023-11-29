90b38b01869792f83acd5a8c3256204522511040
cholewakrzysztof <cholewa.krzysztof24@gmail.com>
2023-11-29 20:50:25 +0100

    Code refactor

--- FrontEnd/src/main/java/pwr/student/FrontEnd/DatabaseResultPrinter.java
+++ FrontEnd/src/main/java/pwr/student/FrontEnd/DatabaseResultPrinter.java
@@ -6,6 +6,29 @@ import java.util.*;
 
 public class DatabaseResultPrinter {
     public static void printTableResult(ResultSet rs,String[] columns) throws Exception {
+        ArrayList<String> col = buildColumnsNameArray(columns);
+
+        ArrayList<MyRow> rows = getListOfRows(rs,col);
+
+        Map<String,Integer> fieldWidth = getSizeOfColumns(rows,col);
+
+        printResizedRows(rows,fieldWidth,col);
+    }
+
+    private static void printResizedRows(ArrayList<MyRow> rows, Map<String,Integer> fieldWidth,ArrayList<String> columns){
+        rows.forEach(row -> {
+            StringBuilder rowString = new StringBuilder();
+            columns.forEach(column -> {
+                try {
+                    rowString.append(resize(row.getString(column),fieldWidth.get(column)));
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+            });
+            System.out.println(rowString);
+        });
+    }
+    private static ArrayList<String> buildColumnsNameArray(String[] columns){
         ArrayList<String> col = new ArrayList<>(Arrays.asList(columns));
         if(col.contains("*")) {
             col.remove("*");
@@ -16,62 +39,36 @@ public class DatabaseResultPrinter {
             col.add("date");
             col.add("description");
         }
-
-        ArrayList<MyRow> copy = getListOfRows(rs,col);
-        MyRow title = new MyRow();
-
-        col.forEach(column -> title.put(column,column));
-        copy.add(0,title);
-
-        Map<String,Integer> fieldWidth = getSizeOfColumns(copy,col);
-
-        copy.forEach(row -> {
-            StringBuilder rowString = new StringBuilder();
-            col.forEach(column -> {
-                try {
-                    rowString.append(resize(row.getString(column),fieldWidth.get(column)));
-                } catch (Exception e) {
-                    throw new RuntimeException(e);
-                }
-            });
-            System.out.println(rowString);
-        });
+        return col;
     }
-
-    private static Map<String, Integer> getSizeOfColumns(ArrayList<MyRow> rows, ArrayList<String> columns) throws Exception{
+    private static Map<String, Integer> getSizeOfColumns(ArrayList<MyRow> rows, ArrayList<String> columns){
             Map<String, Integer> sizes = new HashMap<>();
-            for(MyRow row : rows) {
+            rows.forEach(row ->
                 columns.forEach(colName -> {
-                    String value = null;
-                    try {
-                        value = row.getString(colName);
-                    } catch (Exception e) {
-                        throw new RuntimeException(e);
-                    }
-                    if (sizes.containsKey(colName))
-                        sizes.put(colName, sizes.get(colName) > value.length() ? sizes.get(colName) : value.length());
-                    else
-                        sizes.put(colName,value.length());
-                });
-            }
-
+                        String value = row.getString(colName);
+                        if(sizes.containsKey(colName))
+                            sizes.put(colName, sizes.get(colName) > value.length() ? sizes.get(colName) : value.length());
+                        else
+                            sizes.put(colName,value.length());
+                })
+            );
         return sizes;
     }
 
     private static ArrayList<MyRow> getListOfRows(ResultSet rs,ArrayList<String> columns) throws SQLException {
         ArrayList<MyRow> copy = new ArrayList<>();
 
+        MyRow title = new MyRow();
+        columns.forEach(column -> title.put(column,column));
+        copy.add(title);
+
         while (rs.next()){
             MyRow row = new MyRow();
-            columns.forEach(colName -> {
-                try {
-                    if(rs.findColumn(colName)>-1) {
-                        row.put(colName,rs.getString(colName));
-                    }
-                } catch (SQLException e) {
-                    throw new RuntimeException(e);
-                }
-            });
+
+            for(String colName : columns)
+                if(rs.findColumn(colName)>-1)
+                    row.put(colName,rs.getString(colName));
+
             copy.add(row);
         }
         return copy;
--- FrontEnd/src/main/java/pwr/student/FrontEnd/MyRow.java
+++ FrontEnd/src/main/java/pwr/student/FrontEnd/MyRow.java
@@ -3,14 +3,8 @@ package pwr.student.FrontEnd;
 import java.util.HashMap;
 
 public class MyRow {
-
     HashMap<String,String> fields = new HashMap<>();
-    public String getString(String key) throws Exception {
-        if(fields.containsKey(key))
-            return fields.get(key);
-        else
-            throw new Exception("Row don't contain key:"+key);
-    }
+    public String getString(String key) { return fields.getOrDefault(key, "no key"); }
     public void put(String key, String value){
         fields.put(key,value);
     }
